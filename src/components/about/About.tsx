114713602e0d60e52391579a9a5d42c9c329d1fb
Kaden <casanave.k@northeastern.edu>
2024-10-08 12:05:40 -0400

    spelling

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -7,12 +7,12 @@ export default function About() {
         <div className="about-section flex">
             <div className="about-content">
                 <img src="/photos/headshot.jpg" alt="Kaden Headshot" className="about-image" />
-                <p>Hi, I'm Kaden. I'm a full-stack engineer with a background in business and a creative, hard-working personality. <br/><br/>
-                    Guided by one of my favorite philosophies, Kaizen, or "change for the better", I'm dedicated to continuous improvement in all my endevours, personally and orginizationally.<br/><br/>
+                <p>
+                Hi, I'm Kaden. I'm a full-stack engineer with a background in business and a creative, hard-working personality.<br/><br/>
 
-In alignment with this principle, I'm enhancing my skillset by learning modern technology tools. These tools enable me to understand and build elegent business solutions in an evolving, technology-dominant business environment.
+Guided by one of my favorite philosophies, Kaizen, or "change for the better," I'm committed to continuous improvement in all my endeavors, both personally and organizationally.<br/><br/>
 
-I'm passionate about contributing to innovative, cutting-edge companies where I can leverage my unique blend of management skills and technical expertise to drive meaningful impact. I'm eager to collaborate with forward-thinking teams and continue my journey of growth and improvement. 
+In line with this principle, I'm expanding my expertise by learning modern technologies. These tools empower me to develop elegant business solutions in an ever-evolving, technology-driven environment. I'm passionate about contributing to innovative, cutting-edge companies where I can leverage my unique blend of management skills and technical knowledge to drive meaningful impact. I'm eager to collaborate with forward-thinking teams and continue my journey of growth and improvement. 
                 </p>
             </div>
         </div>
