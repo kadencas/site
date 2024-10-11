b0bd20d380071954451bb28c50e0c01e27a97061
Kaden <casanave.k@northeastern.edu>
2024-10-11 16:42:46 -0400

    temp

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/portfolio" element={<Weather />} />
           </Routes>
           </div>
         </div>
